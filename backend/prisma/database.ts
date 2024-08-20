20c2bf394b004bedb3adc17ef0cd26b24660b6ca
DanielBlankson <blanksondaniel@yahoo.com>
2024-08-20 11:47:09 +0100

    Setup backend with eslint and prettier

--- backend/prisma/database.ts
+++ backend/prisma/database.ts
@@ -1,35 +1,34 @@
 // Run this script using npx ts-node .\database.ts
 
-import { Pool } from 'pg'
-import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '@prisma/client'
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
 
-const connectionString = `${process.env.DATABASE_URL}`
+const connectionString = `${process.env.DATABASE_URL}`;
 
-const pool = new Pool({ connectionString })
-const adapter = new PrismaPg(pool)
-const prisma = new PrismaClient({ adapter })
-
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
 
 async function main() {
-    // Create a new user
-    const newUser = await prisma.user.create({
-      data: {
-        name: 'Blankson',
-        email: 'blankson@example.com',
-      },
-    });
-    console.log('Created new user:', newUser);
-  
-    // Fetch all users
-    const allUsers = await prisma.user.findMany();
-    console.log('All users:', allUsers);
-  }
-  
-  main()
-    .catch((e) => {
-      throw e;
-    })
-    .finally(async () => {
-      await prisma.$disconnect();
-    });
+  // Create a new user
+  const newUser = await prisma.user.create({
+    data: {
+      name: "Magda",
+      email: "magda@example.com"
+    }
+  });
+  console.log("Created new user:", newUser);
+
+  // Fetch all users
+  const allUsers = await prisma.user.findMany();
+  console.log("All users:", allUsers);
+}
+
+main()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
